3e87c16383908cd8ba4951fdf34d58faf83529d0
Dzhanet <104907844+Dzhanet@users.noreply.github.com>
2022-08-05 00:58:04 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy #импортируем заранее установленную алхимию
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sglite:///database.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #вылезала ошибка в service при True
+db = SQLAlchemy(app)
